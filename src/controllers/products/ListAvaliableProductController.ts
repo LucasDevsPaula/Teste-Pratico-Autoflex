6136e67944e79b4d00ad0a166ae78221e7d6082f
TomoriComAncora <lucas.fernando@ufms.br>
2026-02-10 09:44:35 -0400

    fix: corrigir parâmetro não utilizado no controlador de listagem de produtos

--- src/controllers/products/ListAvaliableProductController.ts
+++ src/controllers/products/ListAvaliableProductController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { ListAvaliableProductService } from "../../services/products/ListAvailableProductsService";
 
 class ListAvaliableProductController {
-  async handle(req: Request, res: Response) {
+  async handle(_req: Request, res: Response) {
     const listAvaliableProductService = new ListAvaliableProductService();
 
     const products = await listAvaliableProductService.execute();
